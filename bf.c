572ed8455db8830e6225cc528c3afe4b9a611714
GOKOP <igorbugajski@protonmail.com>
2021-12-12 23:51:10 +0100

    reading program into memory

--- bf.c
+++ bf.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "stack.h"
 
-int main() {
-	stack_el* stack = NULL;
-	stack_push(&stack, 6);
-	stack_push(&stack, 3);
-	stack_push(&stack, 1);
-	stack_push(&stack, 4);
-	printf("%lu ", stack->val);
-	stack_pop(&stack);
-	printf("%lu ", stack->val);
-	stack_pop(&stack);
-	printf("%lu ", stack->val);
-	stack_pop(&stack);
-	printf("%lu ", stack->val);
-	return 0;
+#define READ_CHUNK 100
+
+// size contains the size of the written buffer, without bytes allocated but not written
+char* load_program(char* fname, size_t* size) {
+	FILE* in = fopen(fname, "r");
+	if(!in) return NULL;
+
+	*size = READ_CHUNK;
+	char* program = malloc((*size) * sizeof(char));
+	size_t offset = 0;
+	int bytes_read = 0;
+
+	while((bytes_read = fread(program + offset, 1, READ_CHUNK, in)) == READ_CHUNK) {
+		*size += READ_CHUNK;
+		offset += READ_CHUNK;
+		program = realloc(program, (*size) * sizeof(char));
+	}
+	*size -= (READ_CHUNK - bytes_read);
+
+	return program;
+}
+
+int main(int argc, char** argv) {
+	if(argc != 2) {
+		printf("Usage: %s <filename>\n", argv[0]);
+		exit(1);
+	}
+
+	size_t bytes_read;
+	char* prog = load_program(argv[1], &bytes_read);
+	if(prog == NULL) printf("dupa\n");
+	else {
+		printf("read bytes: %lu\n", bytes_read);
+		for(size_t i=0; i<bytes_read; ++i) {
+			putchar(prog[i]);
+		}
+		free(prog);
+	}
 }
