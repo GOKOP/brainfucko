093f8e214f2417f25bbef0ef3628573e40ac7f98
GOKOP <igorbugajski@protonmail.com>
2021-12-15 00:03:19 +0100

    errors in repl wont crash the program now

--- bf.c
+++ bf.c
@@ -35,7 +35,9 @@ char* load_program(char* fname) {
 	return program;
 }
 
-void skip_loop(char** ptr) {
+// return value to be treated like a program retrun value (0 is good)
+
+int skip_loop(char** ptr) {
 	// move through the program until the end of the encountered loop
 	// which may contain other loops
 
@@ -48,42 +50,46 @@ void skip_loop(char** ptr) {
 
 	if(loop_cnt > 0) {
 		printf("ERROR: Bad syntax: '[' without a matching ']'\n");
-		exit(2);
+		return 2;
 	}
 
 	--(*ptr); // the loop above moves it past the ']'
 	          // but outside this function it's moved further after switch
+	return 0;
 }
 
-void incr_ptr(uchar* beg, uchar** ptr) {
+int incr_ptr(uchar* beg, uchar** ptr) {
+	if(*ptr >= beg + MEMSIZE - 1) {
+		printf("ERROR: Moved past allowed memory\n");
+		return 3;
+	}
 	++(*ptr);
-	if(*ptr >= beg + MEMSIZE) {
-		printf("ERROR: Moved past allowed memory\n");
-		exit(3);
-	}
+	return 0;
 }
 
-void decr_ptr(uchar* beg, uchar** ptr) {
-	--(*ptr);
-	if(*ptr < beg) {
+int decr_ptr(uchar* beg, uchar** ptr) {
+	if(*ptr < beg+1) {
 		printf("ERROR: Moved past allowed memory\n");
-		exit(3);
+		return 3;
 	}
+	--(*ptr);
+	return 0;
 }
 
 // note that if newline is true then this function will print a newline at the end
 // if the brainfuck program prints anything but not otherwise
-void process_program(char* program, uchar* memory, uchar** mem_ptr, bool newline) {
+int process_program(char* program, uchar* memory, uchar** mem_ptr, bool newline) {
 	stack_el* loops = NULL;
 	char* prog_ptr = program;
 	bool prints = false; // whether or not there were prints
+	int ret = 0;
 
 	while(*prog_ptr) {
 		switch(*prog_ptr) {
 			case '>': 
-				incr_ptr(memory, mem_ptr); break;
+				ret = incr_ptr(memory, mem_ptr); break;
 			case '<': 
-				decr_ptr(memory, mem_ptr); break;
+				ret = decr_ptr(memory, mem_ptr); break;
 			case '+': 
 				++(**mem_ptr); break;
 			case '-': 
@@ -96,21 +102,24 @@ void process_program(char* program, uchar* memory, uchar** mem_ptr, bool newline
 				**mem_ptr = getchar(); break;
 			case '[':
 				if(**mem_ptr) stack_push(&loops, prog_ptr);
-				else skip_loop(&prog_ptr);
+				else ret = skip_loop(&prog_ptr);
 				break;
 			case ']':
 				if(!loops) {
 					printf("ERROR: Bad Syntax: ']' without matching '['\n");
-					exit(2);
+					return 2;
 				} else if(**mem_ptr) prog_ptr = loops->val;
 				else stack_pop(&loops);
 				break;
 			default: break;
 		}
+
+		if(ret > 0) return ret;
 		++prog_ptr;
 	}
 
 	if(newline && prints) printf("\n");
+	return 0;
 }
 
 void run_from_file(char* fname) {
@@ -128,7 +137,8 @@ void run_from_file(char* fname) {
 	memset(memory, 0, MEMSIZE);
 
 	uchar* mem_ptr = memory;
-	process_program(program, memory, &mem_ptr, false);
+	int ret = process_program(program, memory, &mem_ptr, false);
+	if(ret > 0) exit(ret);
 }
 
 void repl() {
