74a489e89eb3a797c8e95d0767e2fa481745e658
GOKOP <igorbugajski@protonmail.com>
2021-12-15 15:52:31 +0100

    movied processing of special commands into a separate function

--- src/repl.c
+++ src/repl.c
@@ -5,6 +5,14 @@
 #include <stdio.h>
 #include <string.h>
 
+// 0 if not a special command
+int process_commands(const char* input, bool* running) {
+	if(input[0] != ':') return 0;
+
+	if(strcmp(input, ":q\n") == 0) *running = false;
+	return 1;
+}
+
 void repl(int mem_size) {
 	uchar* memory = malloc(mem_size * sizeof(uchar));
 	if(!memory) {
@@ -17,12 +25,14 @@ void repl(int mem_size) {
 	char* linebuf = NULL;
 	size_t bufsize = 0;
 	char* err = NULL;
+	bool running = true;
 
-	while(true) {
+	while(running) {
 		printf("%li: %i> ", mem_ptr - memory, *mem_ptr);
 		getline(&linebuf, &bufsize, stdin);
-		if(strcmp(linebuf, ":q\n") == 0) break;
-		process_program(linebuf, memory, &mem_ptr, mem_size, &err, true);
+
+		if(!process_commands(linebuf, &running))
+			process_program(linebuf, memory, &mem_ptr, mem_size, &err, true);
 
 		if(err) {
 			printf("%s\n", err);
