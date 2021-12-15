f27c6ac204d8315e192169e687469d74c3a71895
GOKOP <igorbugajski@protonmail.com>
2021-12-15 17:39:44 +0100

    command line help

--- src/main.c
+++ src/main.c
@@ -63,13 +63,25 @@ void run_from_file(char* fname) {
 	if(ret > 0) exit(ret);
 }
 
+void print_help(const char* bin_name) {
+	printf("Usage: %s <filename>\n", bin_name);
+	printf("       %s --help\n", bin_name);
+	printf("       %s --repl\n", bin_name);
+	printf("Exit codes:\n");
+	printf("0 - success\n");
+	printf("1 - error in the interpreter\n");
+	printf("2 - brainfuck syntax error\n");
+	printf("3 - brainfuck execution error\n");
+}
+
 int main(int argc, char** argv) {
 	if(argc != 2) {
-		printf("Usage: %s <filename>\n", argv[0]);
+		print_help(argv[0]);
 		exit(1);
 	}
 
-	if(strcmp(argv[1], "--repl") == 0) repl(MEMSIZE);
+	if(strcmp(argv[1], "--help") == 0) print_help(argv[0]);
+	else if(strcmp(argv[1], "--repl") == 0) repl(MEMSIZE);
 	else run_from_file(argv[1]);
 	return 0;
 }
--- src/repl.c
+++ src/repl.c
@@ -4,19 +4,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-void print_help() {
+void print_repl_help() {
 	printf("Special commands:\n");
 	printf(":q - quit\n");
 	printf(":h - help\n");
+	printf(":m - print all non-zero memory cells\n");
+}
+
+void print_memory(const uchar* memory, int mem_size) {
+	const uchar* ptr = memory;
+	while(ptr < memory + mem_size) {
+		if(*ptr != 0) printf("%li: %i\n", ptr - memory, *ptr);
+		++ptr;
+	}
 }
 
 // 0 if not a special command
-int process_commands(const char* input, bool* running) {
+int process_commands(const char* input, bool* running, const uchar* memory, int mem_size) {
 	if(input[0] != ':') return 0;
 
 	if(strcmp(input, ":q\n") == 0) *running = false;
-	else if(strcmp(input, ":h\n") == 0) print_help();
+	else if(strcmp(input, ":h\n") == 0) print_repl_help();
+	else if(strcmp(input, ":m\n") == 0) print_memory(memory, mem_size);
 
 	return 1;
 }
@@ -41,7 +52,7 @@ void repl(int mem_size) {
 		printf("%li: %i> ", mem_ptr - memory, *mem_ptr);
 		getline(&linebuf, &bufsize, stdin);
 
-		if(!process_commands(linebuf, &running))
+		if(!process_commands(linebuf, &running, memory, mem_size))
 			process_program(linebuf, memory, &mem_ptr, mem_size, &err, true);
 
 		if(err) {
